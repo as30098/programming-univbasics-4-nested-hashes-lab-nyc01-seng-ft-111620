390c3ba7e60700543e5b41f44d2bc2d61e98b831
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-09 15:47:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,25 +66,8 @@ def adding_matz
 # }
 # return the entire updated hash
   
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-  programmer_hash[:yukihiro_matsumoto] = {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-  puts programmer_hash
+	
+ 
 end
 
 def changing_alan
